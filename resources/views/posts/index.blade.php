f421eb58b45a08accc07ba7f94b8203e040ed722
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-02-15 20:31:25 +0500

    Show post title, author, and created date in index page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <!-- Main content -->
     <main class="flex-grow container mx-auto px-4 py-8">
-        <h1 class="text-3xl font-bold mb-6">All Posts</h1>
+        <h1 class="text-3xl font-bold mb-6">All Posts </h1>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)
 
