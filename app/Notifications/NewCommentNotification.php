0eeb00f546a40a43ab01a22960fcd9d0bca9b0cd
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-02-15 20:16:22 +0500

    Store new comment notifications in database

--- app/Notifications/NewCommentNotification.php
+++ app/Notifications/NewCommentNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-
+//
 class NewCommentNotification extends Notification
 {
     use Queueable;
