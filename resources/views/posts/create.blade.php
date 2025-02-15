a30b9f49072e61c203c40df4b02a458083843fb6
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-02-15 20:29:41 +0500

    Add form for creating new posts with CSRF protection

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -28,7 +28,7 @@
                     @enderror
                 </div>
                 <div class="mb-4">
-                    <label for="image" class="block text-sm font-medium text-gray-700">Image</label>
+                    <label for="image" class="block text-sm font-medium text-gray-700">Image </label>
                     <input type="file" id="image" name="image" value="{{old('image')}}" accept="image/*" required
                         class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
                 </div>
