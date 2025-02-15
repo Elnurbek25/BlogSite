f6ca3a3b74ac9e132357a6ea43cdee98a10c83ff
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-02-15 20:16:52 +0500

    Store new follower notifications in database

--- app/Notifications/NewFollowerNotification.php
+++ app/Notifications/NewFollowerNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-
+//
 class NewFollowerNotification extends Notification
 {
     use Queueable;
